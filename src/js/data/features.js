8b3e72d7da1bfbd65a3a2e30858ca031cde8f062
berefire <berenice.prado.villalobos@gmail.com>
2026-07-08 19:24:07 -0600

    feat: add features data for bookmarking, searching, and sharing

---
+++ src/js/data/features.js
@@ -0,0 +1,41 @@
+export const features = [
+  {
+    id: "bookmarking",
+    tab: "Simple Bookmarking",
+    title: "Bookmark in one click",
+    description:
+      "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
+    image: "/images/illustration-features-tab-1.svg",
+    imageAlt: "Bookmark manager interface",
+    button: {
+      label: "More Info",
+      href: "#",
+    },
+  },
+  {
+    id: "searching",
+    tab: "Speedy Searching",
+    title: "Intelligent search",
+    description:
+      "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
+    image: "/images/illustration-features-tab-2.svg",
+    imageAlt: "Search feature interface",
+    button: {
+      label: "More Info",
+      href: "#",
+    },
+  },
+  {
+    id: "sharing",
+    tab: "Easy Sharing",
+    title: "Share your bookmarks",
+    description:
+      "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
+    image: "/images/illustration-features-tab-3.svg",
+    imageAlt: "Bookmark sharing interface",
+    button: {
+      label: "More Info",
+      href: "#",
+    },
+  },
+];
