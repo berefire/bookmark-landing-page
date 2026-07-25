71ea9c03ac66d60df78e53d4ffa5a618035172e0
berefire <berenice.prado.villalobos@gmail.com>
2026-07-24 19:15:22 -0600

    feat: implement mobile menu functionality with accessibility improvements

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
 import "@styles/input.css";
+
+import { initializeMenu } from "@js/features";
+
+initializeMenu();
---
+++ src/js/shared/constants.js
@@ -0,0 +1,23 @@
+export const BREAKPOINTS = {
+    desktop: "64em"
+};
+
+export const ANIMATION_DURATION = 300; // Duration in milliseconds
+
+export const REQUIRED_ELEMENTS = [
+    // Mobile menu
+    "mobileNavButton",
+    "mobileCloseButton",
+    "mobileNav",
+    "mobileNavContent",
+    "mobileNavLinks",
+
+    // Slider
+    "sliderSource",
+    "sliderImage",
+    "sliderTitle",
+    "sliderDescription",
+
+    "previousSlideButton",
+    "nextSlideButton",
+];
