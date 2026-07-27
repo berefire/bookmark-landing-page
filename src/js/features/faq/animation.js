d6d6fd85e0be4fc16a559f407cd002bcbe92c425
berefire <berenice.prado.villalobos@gmail.com>
2026-07-27 06:47:29 -0600

    refactor: extract FAQ open and close animations into separate module

---
+++ src/js/features/faq/animation.js
@@ -0,0 +1,42 @@
+ function waitTransition(element, property = "height") {
+  return new Promise(resolve => {
+    element.addEventListener(
+      "transitionend",
+      function handle(event) {
+        if (event.propertyName !== property) {
+          return;
+        }
+
+        resolve();
+      },
+      { once: true }
+    );
+  });
+}
+ 
+ export async function animateOpen(item, answer) {
+    item.setAttribute("open", "");
+
+    answer.style.height = "0px";
+    answer.style.opacity = "0";
+
+    requestAnimationFrame(() => {
+      answer.style.height = `${answer.scrollHeight}px`;
+      answer.style.opacity = "1";
+    });
+
+    await waitTransition(answer);
+    answer.style.height = "auto";
+  }
+
+  export async function animateClose(item, answer) {
+    answer.style.height = `${answer.scrollHeight}px`;
+
+    requestAnimationFrame(() => {
+      answer.style.height = "0px";
+      answer.style.opacity = "0";
+    });
+
+    await waitTransition(answer);
+    item.open = false;
+  }
--- src/js/features/faq/controller.js
+++ src/js/features/faq/controller.js
@@ -1,60 +1,25 @@
+import { animateOpen, animateClose } from "./animation.js";
+
 export function createFaqController({ faqItems, faqAnswers }) {
-  function toggle(activeIndex) {
+  async function toggle(activeIndex) {
     const currentItem = faqItems[activeIndex];
     const currentAnswer = faqAnswers[activeIndex];
 
     if (currentItem.open) {
-      animateClose(currentItem, currentAnswer);
+      await animateClose(currentItem, currentAnswer);
       return;
     }
 
-    closeOpenedItems(activeIndex);
-    animateOpen(currentItem, currentAnswer);
+    await closeOpenedItems(activeIndex);
+    await animateOpen(currentItem, currentAnswer);
   }
 
-  function closeOpenedItems(activeIndex) {
-    faqItems.forEach((item, index) => {
+  async function closeOpenedItems(activeIndex) {
+    for (const [index, item] of faqItems.entries()) {
       if (index !== activeIndex && item.open) {
-        animateClose(item, faqAnswers[index]);
+        await animateClose(item, faqAnswers[index]);
       }
-    });
-  }
-
-  function animateOpen(item, answer) {
-    item.setAttribute("open", "");
-
-    answer.style.height = "0px";
-    answer.style.opacity = "0";
-
-    requestAnimationFrame(() => {
-      answer.style.height = `${answer.scrollHeight}px`;
-      answer.style.opacity = "1";
-    });
-
-    answer.addEventListener(
-      "transitionend",
-      function handleOpen() {
-        answer.style.height = "auto";
-        answer.removeEventListener("transitionend", handleOpen);
-      }
-    );
-  }
-
-  function animateClose(item, answer) {
-    answer.style.height = `${answer.scrollHeight}px`;
-
-    requestAnimationFrame(() => {
-      answer.style.height = "0px";
-      answer.style.opacity = "0";
-    });
-
-    answer.addEventListener(
-      "transitionend",
-      function handleClose() {
-        item.removeAttribute("open");
-        answer.removeEventListener("transitionend", handleClose);
-      }
-    );
+    }
   }
 
   return {
